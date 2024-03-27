53700a2b9d4449d99d35a7a8b2770a95d082ed6d
LucasNF22 <lucasnfiorentino@gmail.com>
2024-03-27 14:43:15 -0300

    Creado el contador como componente usando useState

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,8 +1,26 @@
 
+import { CartCounter } from "@/app/shopping-cart";
+
+
+export const metadata = {
+ title: 'Counter',
+ description: 'Contador hecho en cliente',
+};
+
+
+
 export default function CounterPage() {
+
+  
   return (
-    <div>
-      <h1>Counter Page</h1>
+
+    <div className="flex flex-col items-center justify-center w-full h-full">
+
+      <span>Productos en el carrito</span>
+
+      <CartCounter value={ 102 } />
+
+      
     </div>
   );
 }
