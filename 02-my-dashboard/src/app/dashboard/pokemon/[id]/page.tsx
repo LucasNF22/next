365ac4a61474d08ec94f41d3728be2a1e7c88186
LucasNF22 <lucasnfiorentino@gmail.com>
2024-04-03 17:56:17 -0300

    Iniciando fetch y tipado de un pokemon por id

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,15 +1,31 @@
+import { Pokemon } from "@/pokemons";
+
 
 interface Props {
     params: { id: string };
 };
 
+
+const getPokemon = async( id: string ): Promise<Pokemon> => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ id }`,{
+        cache: "force-cache" // va a cambiar mas adelante
+    }).then( resp => resp.json() );
+
+    console.log('Se cargó a ',pokemon.name);
+    
+    return pokemon;
+}
+
+
 export default function PokemonPage({ params }: Props) {
 
     const { id } = params
 
+    const pokemon = getPokemon( id )
+
     return (
         <div>
-            <h1>Pokemon {id}</h1>
+            <h1>Pokemon { id } </h1>
             
         </div>
     );
