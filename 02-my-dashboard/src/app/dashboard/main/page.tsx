eca69ce8fb0d4d79bc3f1df6820cd6e8e8b8ac38
LucasNF22 <lucasnfiorentino@gmail.com>
2024-04-08 21:23:10 -0300

    Inicio de configuracion de store, Providers y counter slice

--- 02-my-dashboard/src/app/dashboard/main/page.tsx
+++ 02-my-dashboard/src/app/dashboard/main/page.tsx
@@ -1,6 +1,12 @@
 import { SimpleWidget } from '../../../components/SimpleWidget';
 
 
+export const metadata = {
+ title: 'Admin Dashboard',
+ description: 'Admin Dashboard',
+};
+
+
 export default function MainPage() {
   return (
     <div className="text-black p-2 flex items-center flex-col w-full">
