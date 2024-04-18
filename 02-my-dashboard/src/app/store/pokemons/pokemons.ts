9b61ea94bb7ca36b9549a9dd0b58b79ade1a9610
LucasNF22 <lucasnfiorentino@gmail.com>
2024-04-18 15:04:44 -0300

    Continuando el alojamiento en el localstorage

--- 02-my-dashboard/src/app/store/pokemons/pokemons.ts
+++ 02-my-dashboard/src/app/store/pokemons/pokemons.ts
@@ -1,36 +1,46 @@
 import { SimplePokemon } from "@/pokemons";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-
 interface PokemonFavState {
-    [key: string]: SimplePokemon,
-
+  [key: string]: SimplePokemon;
 };
 
+const getInitialState = (): PokemonFavState => { 
+  const favorites = JSON.parse( localStorage.getItem( 'favorite-pokemons') ?? "{}" )
+
+  return favorites
+};
 
 const initialState: PokemonFavState = {
-    '1': { id: "1", name: "bulbasaur"},
-    '3': { id: "3", name: "venusaur"},
-    '4': { id: "4", name: "charmander"},
+  ...getInitialState(),
+  // "1": { id: "1", name: "bulbasaur" },
+  // "3": { id: "3", name: "venusaur" },
+  // "4": { id: "4", name: "charmander" },
 };
 
 const pokemonsSlice = createSlice({
-  name: 'pokemons',
+  name: "pokemons",
   initialState,
   reducers: {
-    
-    toogleFavorite( state, action: PayloadAction<SimplePokemon> ){
-        
-        const pokemon = action.payload;
-        const { id } = pokemon;
 
-        if ( !!state[id] ) {
-            delete state[id];
-            return
-        };
+    toogleFavorite(state, action: PayloadAction<SimplePokemon>) {
+      const pokemon = action.payload;
+      const { id } = pokemon;
+
+      if (!!state[id]) {
+        delete state[id];
+        return;
+      } else {
 
         state[id] = pokemon;
-    }
+      }
+
+      // No deberia hacerse en Redux
+      localStorage.setItem("favorite-pokemons", JSON.stringify( state ));
+
+    },
+
+
   },
 });
 
--- 02-my-dashboard/src/components/favorites/FavoritesPokemons.tsx
+++ 02-my-dashboard/src/components/favorites/FavoritesPokemons.tsx
@@ -4,19 +4,36 @@ import { useAppSelector } from "@/app/store";
 import { PokemonsGrid } from "@/pokemons";
 
 import { useState } from "react";
+import { IoHeartOutline } from "react-icons/io5";
 
 
 export const FavoritePokemons = () => {
 
-    const favoritesPokemons = useAppSelector( state => Object.values( state.pokemons ));
-    const [pokemons, setPokemons] = useState( favoritesPokemons )
+    const favoritesPokemons = useAppSelector(state => Object.values(state.pokemons));
+    const [pokemons, setPokemons] = useState(favoritesPokemons)
 
 
     // console.log( favoritesPokemons );
-    
+
 
     return (
-        // <PokemonsGrid pokemons={ favoritesPokemons }/>
-        <PokemonsGrid pokemons={ pokemons }/>
+        <>
+            {
+                pokemons.length === 0
+                    ? ( <NoFavorites /> )
+                    : ( <PokemonsGrid pokemons={pokemons}  /> )
+
+            }
+        </>
+    )
+}
+
+export const NoFavorites = () => {
+    return (
+
+        <div className="flex flex-col w-full h-full items-center justify-center pt-5">
+            <IoHeartOutline size={150} className="text-red-500" />
+            <span>Sin favoritos en colección :(</span>
+        </div>
     )
 }
