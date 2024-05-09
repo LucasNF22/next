b609b2ff6be9d0ee56c66d9aefdd926ab5b4d503
LucasNF22 <lucasnfiorentino@gmail.com>
2024-05-09 20:39:59 -0300

    Fin de layout general

--- 03-admin-todos/src/components/Sidebar.tsx
+++ 03-admin-todos/src/components/Sidebar.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { CiBookmarkCheck, CiLogout } from "react-icons/ci";
 
 import React from 'react'
+import { SidebarItem } from "./SidebarItem";
+import Link from "next/link";
 
 export const Sidebar = () => {
     return (
@@ -9,21 +11,18 @@ export const Sidebar = () => {
         <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
             <div>
                 <div className="-mx-6 px-6 py-4">
-                    {/* TODO: Next/Link hacia dashboard */}
-                    <a href="#" title="home">
-                        {/* Next/Image */}
+                    <Link href={"/dashboard"} title="home">
                         <Image   
                             src={"https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg"}
-                            alt={"tailus logo" }
+                            alt={"logo" }
                             className={"w-32"}
                             width={100}
                             height={100}
                         />
-                    </a>
+                    </Link>
                 </div>
 
                 <div className="mt-8 text-center">
-                    {/* Next/Image */}
                     <Image 
                         src={"https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"} 
                         alt={""}
@@ -35,21 +34,8 @@ export const Sidebar = () => {
                     <span className="hidden text-gray-400 lg:block">Admin</span>
                 </div>
 
-                <ul className="space-y-2 tracking-wide mt-8">
-                    {/* TODO: src/components <SidebarItem /> */}
-                    {/* Active className: text-white bg-gradient-to-r from-sky-600 to-cyan-400 */}
-                    <li>
-                        <a href="#" className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
-                            <CiBookmarkCheck size={30} />
-                            <span className="-mr-1 font-medium">Dashboard</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                            <CiBookmarkCheck size={30} />
-                            <span className="group-hover:text-gray-700">Categories</span>
-                        </a>
-                    </li>
+                <ul className="space-y-2 tracking-wide mt-8">                  
+                    <SidebarItem />
                 </ul>
             </div>
 
--- 03-admin-todos/src/components/index.ts
+++ 03-admin-todos/src/components/index.ts
@@ -1,2 +1,8 @@
 
-export { Sidebar } from '../components/Sidebar';
+
+export { Sidebar } from './Sidebar';
+export { SidebarItem } from './SidebarItem';
+
+export { TopMenu } from './TopMenu';
+
+export { WidgetItem } from './WidgetItem';
