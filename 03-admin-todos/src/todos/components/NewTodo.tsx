5aafcd01067a7f9116baf4cdb25d2182268c26e3
LucasNF22 <lucasnfiorentino@gmail.com>
2024-06-13 10:48:38 -0300

    Continuando newTodo

--- 03-admin-todos/src/todos/components/NewTodo.tsx
+++ 03-admin-todos/src/todos/components/NewTodo.tsx
@@ -1,12 +1,20 @@
 'use client';
 
+import { FormEvent } from "react";
 import { IoTrashOutline } from "react-icons/io5";
 
 
 export const NewTodo = () => { 
 
+  const onSubmit = async( e: FormEvent ) => {
+    e.preventDefault();
+
+    console.log('form submitted');
+    
+  }
+
   return (
-    <form  className='flex w-full'>
+    <form onSubmit={ onSubmit } className='flex w-full'>
       <input type="text"
         className="w-6/12 -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-sky-500 transition-all"
         placeholder="¿Qué necesita ser hecho?" />
