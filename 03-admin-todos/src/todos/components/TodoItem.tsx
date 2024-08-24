181e570cd32dbf6b8ace6eed9b619f6dfc953d94
LucasNF22 <lucasnfiorentino@gmail.com>
2024-08-24 16:48:18 -0300

    Server Actions - OptimisticLoads

--- 03-admin-todos/src/todos/components/TodoItem.tsx
+++ 03-admin-todos/src/todos/components/TodoItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { startTransition, useOptimistic } from "react";
 import { Todo } from "@prisma/client"
 
 import styles from "./TodoItem.module.css";
@@ -8,36 +9,54 @@ import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
 interface Props {
     todo: Todo,
     //acciones que hay llamar
-    toggleTodo: (id: string, complete: boolean ) => Promise<Todo|void>
+    toggleTodo: (id: string, complete: boolean) => Promise<Todo | void>
 }
 
 
 export const TodoItem = ({ todo, toggleTodo }: Props) => {
 
-  return (
-    <div className={ todo.complete ? styles.todoDone : styles.todoPending }>
-        <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
-            <div 
-                onClick={ () => toggleTodo( todo.id, !todo.complete ) }
-                className={`
+    const [ todoOptimistic, toggleTodoOptimistic ] = useOptimistic(
+        todo, 
+        ( state, newCompleteValue: boolean ) => ({ ...state, complete: newCompleteValue })
+    );
+
+    const onToggleTodo = async() => {
+        try {
+            startTransition( () => toggleTodoOptimistic( !todo.complete ) )
+            
+            await toggleTodo( todoOptimistic.id, !todoOptimistic.complete )
+
+        } catch (error) {
+            startTransition( () => toggleTodoOptimistic( !todo.complete ) )
+        }
+    }
+
+
+    return (
+        <div className={todoOptimistic.complete ? styles.todoDone : styles.todoPending}>
+            <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
+                <div
+                    // onClick={() => toggleTodo(todoOptimistic.id, !todoOptimistic.complete)}
+                    onClick={ onToggleTodo }
+                    className={`
                     flex p-2 rounded-md cursor-pointer
                     hover:bg-opacity-60
-                    ${todo.complete ? 'bg-blue-100' : 'bg-red-100'}
+                    ${todoOptimistic.complete ? 'bg-blue-100' : 'bg-red-100'}
                 `}>
-                {
-                    todo.complete 
-                    ? <IoCheckboxOutline size={ 30 }/>
-                    : <IoSquareOutline size={ 30 }/>
-                }
-                
-            </div>
+                    {
+                        todoOptimistic.complete
+                            ? <IoCheckboxOutline size={30} />
+                            : <IoSquareOutline size={30} />
+                    }
 
-            <div className="text-center sm:text-left">
-                { todo.description }
+                </div>
+
+                <div className="text-center sm:text-left">
+                    {todo.description}
+                </div>
+
             </div>
 
         </div>
-        
-    </div>
-  )
+    )
 }
