f8992e8296e6419fb8d9facb66ccce193d1b2002
LucasNF22 <lucasnfiorentino@gmail.com>
2024-09-09 17:41:21 -0300

    Se construyo la logica para agregar y quitar productos en las cookies, al igual que mostrar la cantidad total de productos en el topMenu

---
+++ 03-admin-todos/src/app/dashboard/cart/page.tsx
@@ -0,0 +1,20 @@
+
+
+export const metadata = {
+ title: 'Carrito de compras',
+ description: 'SEO Title',
+};
+
+
+export default function CartPage() {
+  return (
+    <div>
+      <h1 className="text-5xl">Carrito de compras</h1>
+      <hr className="mb-2"/>
+
+      <div className="flex flex-col sm:flex-row gap-2 w-full">
+
+      </div>
+    </div>
+  );
+}
---
+++ 03-admin-todos/src/shopping-cart/actions/actions.ts
@@ -0,0 +1,46 @@
+// En el lado del cliente
+
+import { deleteCookie, getCookie, hasCookie, setCookie } from "cookies-next";
+
+/*
+
+Cookie = cart: 
+{
+    'uui-123-1: 4
+    'uui-123-2: 1
+    'uui-123-3: 3
+}
+
+*/
+
+export const getCookieCart = ():{ [id: string]: number } => {
+
+    if( hasCookie ('cart')){
+        const cookieCart= JSON.parse( getCookie('cart') as string ?? '{}' );
+        return cookieCart;
+    };
+
+    return {};
+};
+
+export const addProductToCart = ( id: string) => {
+
+    const cookieCart = getCookieCart();
+
+    if( cookieCart[id] ){     // Si existe
+        cookieCart[id] += 1;  // Le suma 1 a la cantidad
+    }else {
+        cookieCart[id] = 1;   // sino, lo agrega con 1.
+    }
+
+    setCookie( 'cart', JSON.stringify( cookieCart ) );
+};
+
+export const deleteProductFromCart = ( id: string) => {
+
+    const cookieCart = getCookieCart();
+
+    delete cookieCart[id];
+
+    setCookie( 'cart', JSON.stringify( cookieCart ) );
+};
