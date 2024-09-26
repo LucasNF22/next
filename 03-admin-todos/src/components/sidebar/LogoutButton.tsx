114a25acf23384e9395306daae2e60b1f6796acd
LucasNF22 <lucasnfiorentino@gmail.com>
2024-09-26 17:31:01 -0300

    Se completo boton para login y logout con su respectiva logica

--- 03-admin-todos/src/components/sidebar/LogoutButton.tsx
+++ 03-admin-todos/src/components/sidebar/LogoutButton.tsx
@@ -1,23 +1,49 @@
-import { useSession } from "next-auth/react";
+'use client'
+
+
+import { useSession, signOut, signIn } from "next-auth/react";
 import { CiLogout } from "react-icons/ci";
 import { IoShieldOutline } from "react-icons/io5";
 
 
 
 
-export const LogoutButton = () =>{
+export const LogoutButton = () => {
 
     const { data: session, status } = useSession();
+    console.log({ status });
 
-    if ( status === 'loading'){
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-            <IoShieldOutline />
-            <span className="group-hover:text-gray-700">Logout</span>
-        </button>
-    }
+    if (status === 'loading') {
+        return (
+
+            <button 
+                className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+                                >
+                <IoShieldOutline />
+                <span className="group-hover:text-gray-700">Comprobando</span>
+            </button>
+        )
+    };
+
+    if (status === 'unauthenticated') {
+        return (
+
+            <button
+                className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+                onClick={() => signIn()}
+            >
+                <CiLogout />
+                <span className="group-hover:text-gray-700">Ingresar</span>
+            </button>
+        )
+    };
+
 
     return (
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+        <button
+            className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+            onClick={() => signOut()}
+        >
             <CiLogout />
             <span className="group-hover:text-gray-700">Logout</span>
         </button>
